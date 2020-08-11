4619b67ccaf0a28a084dac6c4dd1067b095e5646
168504524@qq.com <168504524@qq.com>
2020-08-11 22:53:13 +0800

    好好学习，天天向上>>>>>

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ yag = yagmail.SMTP(
     port='465')
 contents = ['尊敬的某某领导：',
             '接口自动化已执行已完成，请查收附件']
+
 yag.send(to='168504524@qq.com',
          cc='188368844@qq.com',
          subject='市南项目报告',
