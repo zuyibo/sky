5c9cf746b0144ebc59ec7950994e92c91e9602b0
168504524@qq.com <168504524@qq.com>
2020-08-18 00:09:40 +0800

    8.18更新

--- xxi/demo_a.py
+++
@@ -1,8 +0,0 @@
-import traceback
-while True:
-    num = input('input a number:')
-    try:
-        print('100/%s = %s' % (num, 100.0 / int(num)))
-
-    except BaseException:
-        print('我异常了', traceback.format_exc())
