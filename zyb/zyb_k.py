0847b4956d341415657aef1cfa5111f5071dad32
zuyibo <168504524@qq.com>
2020-09-01 00:49:01 +0800

    2020.9.1更新

---
+++ zyb/zyb_k.py
@@ -0,0 +1,16 @@
+import requests
+Host = 'http://127.0.0.1:80'
+api_url = f'{Host}/api/mgr/sq_mgr/'
+header = {'Content-Type':'application/x-www-form-urlencoded'}
+import re
+payload = {
+        'action':'add_course',
+        'data':'{"name":"初中化学",'
+               '"desc":"初中化学课程",'
+               '"display_idx":"4"}'
+}
+
+
+reps = requests.post(api_url,json=payload)
+reps.encoding = 'unicode_escape'
+print(reps.text)
